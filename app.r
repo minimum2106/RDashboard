0ae3f65c0b8e4b11cf5b2f633a34cf9193151f5f
minimum2106 <tranhongquan2106@gmail.com>
2023-02-27 21:15:45 +0100

    add a simple panel system

--- app.r
+++ app.r
@@ -1,10 +1,33 @@
 library(shiny)
 library(shinydashboard)
 
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+             badgeLabel = "new", badgeColor = "green")
+  )
+)
+
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            h2("Dashboard tab content")
+    ),
+    
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+    )
+  )
+)
+
+# Put them together into a dashboardPage
 ui <- dashboardPage(
   dashboardHeader(title = "RDashboard"),
-  dashboardSidebar(),
-  dashboardBody()
+  sidebar,
+  body
 )
 
 server <- function(input, output) { }
