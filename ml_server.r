bb8c4f1fb0986b50ca12575e59c76f9657c03c72
minimum2106 <tranhongquan2106@gmail.com>
2023-03-16 21:43:08 +0100

    add base for svm and fix choose model component prob

--- ml_server.r
+++ ml_server.r
@@ -1,19 +1,18 @@
 library(shiny)
+library(e1071)
 
 train_dataset <- reactiveVal()
 test_dataset <- reactiveVal()
 
+model <- reactiveVal()
+
 observe({
   cols <- colnames(temporary_dataset$data)
+  cols_list <- as.list(strsplit(paste(cols, collapse = ", "), ","))[[1]]
   
   updateSelectInput(
     session, inputId = "ml_target_options",
-    choices = cols
-    )
-  
-  updateSelectInput(
-    session, inputId = "ml_var_options",
-    choices = cols
+    choices = cols_list
     )
 })
 
@@ -21,20 +20,107 @@ observeEvent(
   input$ml_spliting_options, 
   {
     req(input$data_file)
+ 
+  }
+)
+
+observeEvent(
+  input$ml_target_options,
+  {
+    cols <- colnames(temporary_dataset$data)
+    cols_list <- as.list(strsplit(paste(cols, collapse = ", "), ","))[[1]]
+
+    updateSelectInput(
+      session, inputId = "ml_var_options",
+      choices = cols_list[cols_list != input$ml_target_options]
+    )
     
-    if (input$ml_spliting_options == "Cross Validation") {
-      
-    }
+  }
+)
+
+
+observeEvent(
+  input$ml_run_model,
+  {
+    req(input$data_file)
+
+    sample <- sample(
+      c(TRUE, FALSE), 
+      nrow(temporary_dataset$data), 
+      replace=TRUE, prob=c(input$ml_train_percentage, 1 - input$ml_train_percentage)
+    )
     
-    if (input$ml_spliting_options == "Train Test Validation") {
-      sample <- sample(
-        c(TRUE, FALSE), 
-        nrow(temporary_dataset$data), 
-        replace=TRUE, prob=c(input$ml_train_percentage, 1 - input$ml_train_percentage)
-      )
-      
-      train_dataset(temporary_dataset$data[sample, ]) 
-      test_dataset(temporary_dataset$data[!sample, ])
+    data_to_process <- temporary_dataset$data %>% 
+      select(.dots = input$ml_var_options)
+    
+    data_to_process[, input$ml_target_options] <- as.factor(mtcars[, input$ml_target_options])
+    
+    train_dataset(data_to_process[sample, ]) 
+    test_dataset(data_to_process[!sample, ])
+    
+    if (input$ml_model_options == "SVM") {
+      tryCatch({
+        if (input$ml_spliting_options == "Train Test Validation") k = 0 else k = input$ml_cross_validation_k
+        
+        if (input$ml_svm_kernel == "linear") {
+          svm_model <- svm(
+            !!input$ml_target_options ~ .,
+            data = train_dataset(),
+            kernel = input$ml_svm_kernel,
+            cost = input$ml_svm_cost,
+            scale = TRUE,
+            cross = k
+          )
+        }
+        
+        if (input$ml_svm_kernel == "polynomial") {
+          svm_model <- svm(
+            !!input$ml_target_options ~ .,
+            data = train_dataset(),
+            kernel = input$ml_svm_kernel,
+            cost = input$ml_svm_cost,
+            degree = input$ml_svm_degree,
+            gamma = input$ml_svm_gamma,
+            coef0 = input$ml_svm_coef0,
+            scale = TRUE,
+            cross = k
+          )
+        }
+        
+        if (input$ml_svm_kernel == "radial basis") {
+          svm_model <- svm(
+            !!input$ml_target_options ~ .,
+            data = train_dataset(),
+            kernel = input$ml_svm_kernel,
+            cost = input$ml_svm_cost,
+            gamma = input$ml_svm_gamma,
+            scale = TRUE,
+            cross = k
+          )
+        }
+        
+        if (input$ml_svm_kernel == "sigmoid") {
+          svm_model <- svm(
+            !!input$ml_target_options ~ .,
+            data = train_dataset(),
+            kernel = input$ml_svm_kernel,
+            cost = input$ml_svm_cost,
+            gamma = input$ml_svm_gamma,
+            coef0 = input$ml_svm_coef0,
+            scale = TRUE,
+            cross = k
+          )
+        }
+        
+        model(svm_model)
+      }, error = function(e) {print(e)})
     }
+  }
+)
+
+observeEvent(
+  input$ml_run_model,
+  {
+    print("model models")
   }
 )
--- ml_tab.r
+++ ml_tab.r
@@ -3,14 +3,71 @@ library(shiny)
 source("ui_common.r", local = TRUE)
 
 sidebar_ml_split_percentage <-  conditionalPanel(
-  condition = "input.ml_spliting_options == 'Train Test Validation'",
+  condition = "input.ml_spliting_options == 'Cross Validation'",
   numericInput(
-    "ml_train_percentage", 
-    "Training Dataset Percentage:", 
-    value = 0.7, 
-    min = 0, 
-    max = 1, 
-    step = 0.1)
+    "ml_cross_validation_k",
+    "Number of K:",
+    value = 4,
+  )
+ 
+)
+
+# sidebar_ml_cross_validation <- conditionalPanel(
+#   condition = "input.ml_spliting_options == 'Cross Validation'",
+# )
+
+sidebar_ml_svm_degree <- conditionalPanel(
+  condition = "input.ml_model_options == 'SVM' & input.ml_svm_kernel == 'polynomial'",
+  numericInput(
+    "ml_svm_degree",
+    "Degree",
+    value = 3,
+  ),
+)
+
+sidebar_ml_svm_gamma <- conditionalPanel(
+  condition = "input.ml_svm_kernel != 'linear'",
+  numericInput(
+    "ml_svm_gamma",
+    "Gamma",
+    value = 0.2,
+    min = 0.1,
+    max = 1,
+    step = 0.1
+  ),
+)
+
+sidebar_ml_svm_coef0 <- conditionalPanel(
+  condition = "input.ml_svm_kernel == 'polynomial' || input.ml_svm_kernel == 'sigmoid'",
+  numericInput(
+    "ml_svm_coef0",
+    "Coefficient",
+    value = 0,
+  ),
+)
+
+sidebar_ml_svm <- conditionalPanel(
+  condition = "input.ml_model_options == 'SVM'",
+  selectInput(
+    "ml_svm_kernel",
+    "Kernel:",
+    choices = c(
+      "linear",
+      "polynomial",
+      "radial basis",
+      "sigmoid"
+    )
+  ),
+  
+  numericInput(
+    "ml_svm_cost",
+    "Cost",
+    value = 1,
+  ),
+  
+  sidebar_ml_svm_degree,
+  sidebar_ml_svm_gamma,
+  sidebar_ml_svm_coef0
 )
 
 sidebar_ml <- sidebarPanel(
@@ -49,7 +106,18 @@ sidebar_ml <- sidebarPanel(
     choices = c("Train Test Validation", "Cross Validation")
   ),
   
+  numericInput(
+    "ml_train_percentage", 
+    "Training Dataset Percentage:", 
+    value = 0.7, 
+    min = 0, 
+    max = 1, 
+    step = 0.1
+  ),
+  
   sidebar_ml_split_percentage,
+  
+  sidebar_ml_svm,
   
   actionButton(
     "ml_run_model", "Run Model", width = "100%",
@@ -58,8 +126,16 @@ sidebar_ml <- sidebarPanel(
   
 )
 
+ml_content <- tabsetPanel(
+  type = "tabs",
+  tabPanel("Summary", h3("hi")),
+  tabPanel("Predict", h3("hello")),
+  tabPanel("Visualizations", plotOutput("ml_viz")),
+  id = "ml_panels"
+)
+
 
 ml_tab <-  sidebarLayout(
   sidebar_ml,
-  mainPanel()
+  mainPanel(ml_content)
 )
